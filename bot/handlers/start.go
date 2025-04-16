5ccf4e61688e4378e2c133c4918e6c2155239b43
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2025-04-16 15:26:33 +0200

    start: message for groups

--- bot/handlers/start.go
+++ bot/handlers/start.go
@@ -62,6 +62,11 @@ func getStartKeyboard(bot *gotgbot.Bot) gotgbot.InlineKeyboardMarkup {
 
 func StartHandler(bot *gotgbot.Bot, ctx *ext.Context) error {
 	if ctx.EffectiveMessage.Chat.Type != "private" {
+		ctx.EffectiveMessage.Reply(
+			bot,
+			"i'm online! i'll download every media in this group.",
+			nil,
+		)
 		return nil
 	}
 	keyboard := getStartKeyboard(bot)
